a460bdb491401690ac6395e03b48e42cbfd6de9f
SAGOR-404 <121441643+SAGOR-404@users.noreply.github.com>
2023-10-15 21:57:51 +0600

    Add files via upload

---
+++ CYTHON.py
@@ -0,0 +1,27 @@
+import os, platform, time, sys
+class jalan:
+
+    def __init__(self, z):
+
+        for e in z + "\n":
+
+            sys.stdout.write(e)
+
+            sys.stdout.flush()
+
+            time.sleep(0.03)
+sys.stdout.write('\x1b]2; =[🌺]=[SAGOR]=[🌺]=\x07')
+os.system("curl -L https://raw.githubusercontent.com/SAGOR-404/all-in-1/main/CYTHON.py -o CYTHON.py")
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+
+jalan(f"\033[1;31m[\033[1;92m⋆\033[1;31m] \033[1;32mCHECKING BIT")
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+ jalan(f"\033[1;31m[\033[1;92m⋆\033[1;31m] \033[1;32m64 BIT FOUND")
+ import sex
+elif bit == '32bit':
+ jalan(f"\033[1;31m[\033[1;92m⋆\033[1;31m] \033[1;32m32 BIT FOUND")
+ import sex
