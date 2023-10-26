ccb4025f97508ddd1a8c64101ced941c349d1dc2
SAGOR-404 <121441643+SAGOR-404@users.noreply.github.com>
2023-10-26 17:36:58 +0600

    Add files via upload

---
+++ dec_bolod.py
@@ -0,0 +1,86 @@
+"""INFINITY-999 's fake decompile sc fucked by ahmed-xd"""
+
+import os,zlib
+
+
+
+
+from os import system as osRUB
+from os import system as cmd
+os.system('clear')
+print(' Instaling Missing Modules ...\n')
+print(' Wait A Fiew Minutes ...\n')
+
+try:
+    import requests 
+except ImportError:
+    print('\n  installing Requests ...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n  installing futures ...\n')
+    os.system('pip install futures')
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as sarfrazssb
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit
+
+def bot():
+	"""data capture bolod"""
+
+def x():
+	print(" Sorry your device not support this tools")
+
+logo = ("""\033[1;92m  
+    _____   _____________   ______________  __
+   /  _/ | / / ____/  _/ | / /  _/_  __/\ \/ /
+   / //  |/ / /_   / //  |/ // /  / /    \  / 
+ _/ // /|  / __/ _/ // /|  // /  / /     / /  
+/___/_/ |_/_/   /___/_/ |_/___/ /_/     /_/   
+┏════════════════════════════════════════┓
+┃ TOOL OWNER  => INFINITY                ┃
+┃ TOOL NAME   => DEC PYTHON3 & CYTHON    ┃
+┃ TOOL STATUS => FREE/PAID               ┃
+┃ GITHUB      => INFINITY-999            ┃
+┗════════════════════════════════════════┛""")
+ 
+def main():
+    os.system('clear')
+    print(logo)
+    print(f"\033[1;91m\r      DON'T USE A WRONG WORK !! ")
+    print("\033[92;1m═════════════════════════════════════════")
+    print(f"\033[1;97m[\033[92;1m1\033[1;97m]\033[1;97m Dec Marshal 3.11 \033[91;1m   [FREE]")
+    print(f"\033[1;97m[\033[92;1m2\033[1;97m]\033[1;97m Dec Zlib          \033[91;1m  [Free]")
+    print(f"\033[1;97m[\033[92;1m3\033[1;97m]\033[1;97m Dec Bash32       \033[91;1m   [Free]")
+    print(f"\033[1;97m[\033[92;1m4\033[1;97m]\033[1;97m Dec Bash64        \033[91;1m  [Free]")
+    print(f"\033[1;97m[\033[92;1m5\033[1;97m]\033[1;97m Dec Cython        \033[92;1m  [Paid]")
+    print(f"\033[1;97m[\033[92;1m6\033[1;97m]\033[1;97m Dec Cythonize -i   \033[92;1m [Paid]")
+    print(f"\033[1;97m[\033[92;1m7\033[1;97m]\033[1;97m Dec Cythonize -b  \033[92;1m  [Paid]")
+    print(f"\033[1;97m[\033[92;1m8\033[1;97m]\033[1;97m Dec Cythonize -a   \033[92;1m [Paid]")
+    print(f"\033[1;97m[\033[92;1m9\033[1;97m]\033[1;97m Dec Cythonize -i -2\033[92;1m [Paid]")
+    print("═════════════════════════════════════════")
+
+    sh = input("\033[0;97m[\033[38;5;208m?\033[0;97m]\033[0;97mChoice Menu : ")
+    if sh =='1':
+       main()
+    if sh =='2':
+       main()
+    if sh =='3':
+       main()  
+       
+bot()
+main()
